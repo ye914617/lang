bd90471c0d26e961fd13143fd172cf891d6caebe
Covid19 <hailcovid19@gmail.com>
2021-08-08 17:06:43 +0800

    changed navbar logo from <a> to <Link>

--- src/components/Navbar/NavbarLogo.js
+++ src/components/Navbar/NavbarLogo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "./logo.png";
+import { Link } from "react-router-dom";
 
 const NavbarLogo = () => {
   return (
     <div>
-      <a href="/">
+      <Link exact to="/">
         <img src={logo} alt="navbar-logo" />
-      </a>
+      </Link>
     </div>
   );
 };
